47b8fb1563a0844ee8ff828878ab2feb98ce16bf
Steve Bilogan <steve.bilogan@nventive.com>
2016-04-27 13:13:12 +0000

    [iOS] Update GoogleMediaFramework, fixes issue with hidden player controls. Add tracking for more binding files
    work-items: #21160, #21162
    git-tfs-id: [http://tfs04:8080/tfs/Umbrella]$/Umbrella/Umbrella/Projects;C8650

---
+++ ObjC/GoogleMediaFramework/GoogleMediaFramework.m
@@ -0,0 +1,13 @@
+//
+//  GoogleMediaFramework.m
+//  GoogleMediaFramework
+//
+//  Created by Steve Bilogan on 2016-02-23.
+//  Copyright © 2016 Steve Bilogan. All rights reserved.
+//
+
+#import "GoogleMediaFramework.h"
+
+//@implementation GoogleMediaFramework
+
+//@end
