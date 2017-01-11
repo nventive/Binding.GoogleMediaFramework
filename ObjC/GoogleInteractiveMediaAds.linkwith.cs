8049189544e233692b877583837818d6c9617019
Steve Bilogan <steve.bilogan@nventive.com>
2017-01-11 10:08:34 -0500

    Keep zip of entire xcode project

--- ObjC/GoogleInteractiveMediaAds.linkwith.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith("GoogleInteractiveMediaAds.a", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, SmartLink = false, ForceLoad = false, LinkerFlags = "-ObjC -w -lz", Frameworks = "AdSupport AudioToolbox AVFoundation CoreFoundation CoreGraphics MessageUI SystemConfiguration CoreMedia WebKit UIKit MediaPlayer QuartzCore")]
-
